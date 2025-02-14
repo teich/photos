02515677f43ae8a82daec1c41aba964538064c32
Oren Teich <oren@teich.net>
2025-02-14 12:49:12 -0800

    speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Photos',
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
